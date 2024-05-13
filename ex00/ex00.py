9cc3bfb3c7bd0ff61568c06421ded53ef42582a8
pixweez <148324914+pixweez@users.noreply.github.com>
2024-05-13 12:12:58 +0200

    done

---
+++ ex00/ex00.py
@@ -0,0 +1,9 @@
+km = 1.60934
+def convert():
+    miles = float (input ("entrer valeur en miles"))
+    conversion = miles * km
+    print(f"une fois convertit en km, cela fait {conversion} km ")
+
+convert()
+
+bonjour
